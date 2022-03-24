2e7a285b2b5ce11e63463088230873622612894d
tahafatimaDO <82136225+tahafatimaDO@users.noreply.github.com>
2022-03-24 21:46:17 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
 import time
 
 print("Print now")
-time.sleep(45)
-print("Printing after 4.2 seconds")
+n=0
+while(n<10):
+  time.sleep(45)
+  print("Printing after 45 seconds")
+  n=n+1
+  print("value of n is",n)
+
+
